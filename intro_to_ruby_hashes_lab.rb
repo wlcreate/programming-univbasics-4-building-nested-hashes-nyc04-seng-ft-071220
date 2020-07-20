81e7d196ecc6342b66caacd510d4f0baf2f6508b
Waverley Leung <waverley.nyc@gmail.com>
2020-07-20 14:10:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,7 +37,8 @@ def monopoly_with_third_tier
       :rent_in_dollars => {
         :one_piece_owned => "25",
         :two_pieces_owned => "50",
-        :three_pieces_owned => ""
+        :three_pieces_owned => "100",
+        :
       },
       :names => {
         
