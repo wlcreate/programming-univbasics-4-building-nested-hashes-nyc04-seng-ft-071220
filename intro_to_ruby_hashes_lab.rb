e6b9a7efedb085334c6ea17d4b214239cb6df3fb
Waverley Leung <waverley.nyc@gmail.com>
2020-07-20 14:14:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -66,8 +66,8 @@ def monopoly_with_fourth_tier
       },
       :names => {
         :reading_railroad => {:mortgage_value => 100},
-        :pennsylvania_railroad => {:mortgage_value => },
-        :b_and_o_railroad => {},
+        :pennsylvania_railroad => {:mortgage_value => 200},
+        :b_and_o_railroad => {:mortgage_value => },
         :shortline_railroad => {}
       }
     }
