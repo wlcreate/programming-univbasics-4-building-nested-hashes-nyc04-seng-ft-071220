79251dcaa8f3f6c23e2324bde9db1949b27f5eca
Waverley Leung <waverley.nyc@gmail.com>
2020-07-20 14:03:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -2,7 +2,9 @@ def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
   hash = {
-    railroad: {}
+    :railroad => {
+      
+    }
   }
 end
 
